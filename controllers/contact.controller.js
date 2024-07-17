3436c2b10e632c19819036f47965b9ffeb738476
Sébastien <seb.juillet987@hotmail.fr>
2024-07-17 14:00:42 +0200

    error management

--- controllers/contact.controller.js
+++ controllers/contact.controller.js
@@ -1,41 +1,49 @@
+const { StatusCodes } = require("http-status-codes");
 const Contact = require("../models/contact");
+const catchAsync = require("../helpers/catchAsync");
 
-const create = async (req, res) => {
+const create = catchAsync (async (req, res) => {
     const contact = await Contact.create(req.body);
     res.send(contact);
-};
+});
 
-const getAll = async (req, res) => {
+const getAll = catchAsync (async (req, res) => {
     const contacts = await Contact.find();
     res.send(contacts);
-};
+});
 
-const getById = async (req, res) => {
+const getById = catchAsync (async (req, res) => {
     const contact = await Contact.findById(req.params.id);
     if (contact) {
         res.send(contact);
     } else {
-        res.status(404).send("Contact not found");
+        res
+            .status(StatusCodes.NOT_FOUND)
+            .send("Contact not found");
     }
-};
+});
 
-const updateById = async (req, res) => {
+const updateById = catchAsync (async (req, res) => {
     const contact = await Contact.findByIdAndUpdate(req.params.id, req.body, {new: true});
     if (contact) {
         res.send(contact);
     } else {
-        res.status(404).send("Contact not found");
+        res
+            .status(StatusCodes.NOT_FOUND)
+            .send("Contact not found");
     }
-};
+});
 
-const deleteById = async (req, res) => {
+const deleteById = catchAsync (async (req, res) => {
     const contact = await Contact.findByIdAndDelete(req.params.id);
     if (contact) {
         res.send(contact);
     } else {
-        res.status(404).send("Contact not found");
+        res
+            .status(StatusCodes.NOT_FOUND)
+            .send("Contact not found");
     }
-};
+});
 
 module.exports = {
     create,
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const { StatusCodes} = require("http-status-codes"); //package qui permet de gérer les erreurs
 const contactRoutes = require("./routes/contact.routes");
 
 const app = express();
@@ -14,10 +15,18 @@ mongoose.connect("mongodb://localhost/carnet-adresses");
 app.use("/contacts", contactRoutes);
 
 app.use((req, res) => {
-  res.status(404).send("Page introuvable");
+  res
+    .status(StatusCodes.NOT_FOUND)
+    .send("Page introuvable");
 });
 
+app.use((err, req, res, next) => {
+  console.log(err);
+  res
+    .status(StatusCodes.INTERNAL_SERVER_ERROR)
+    .send("Erreur serveur");
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
-})
+});
